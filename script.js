9266c7104a1f69a23ee9e5ee57fabc24cfb20433
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-03-03 15:54:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+document.getElementById("submit").addEventListener("click", (e) => {
+    e.preventDefault();
+    const fName = document.getElementById("firstName").value;
+    const lName = document.getElementById("lastName").value;
+    const pNo = document.getElementById("phoneNumber").value;
+    const email = document.getElementById("email").value;
+    alert(`First Name: ${fName} Last Name: ${lName} Phone Number: ${pNo} Email ID: ${email}`);
+});
